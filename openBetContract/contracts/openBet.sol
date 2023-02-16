a1335da818ae3659ea6e6bf541896b6c149db68c
CLOVETHEDESTROYER <90019717+CLOVETHEDESTROYER@users.noreply.github.com>
2023-02-15 23:34:30 -0700

    updated Smartcontract to include metadata and permissions from ThirdWeb

--- openBetContract/contracts/openBet.sol
+++ openBetContract/contracts/openBet.sol
@@ -1,27 +1,40 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/security/Pausable.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@thirdweb-dev/contracts/extension/Ownable.sol";
+import "@thirdweb-dev/contracts/extension/Permissions.sol";
 
-contract openBet {
+contract openBet is Ownable, Permissions, Pausable {
     address payable custodialWallet;
     address payable payableOwner;
-    address owner;
-    address payable player1;
-    address payable player2;
-    uint256 player1Bet;
-    uint256 player2Bet;
-    uint256 totalBet;
+    address payable public owner;
+    address payable public player1;
+    address payable public player2;
+    uint256 public player1Bet;
+    uint256 public player2Bet;
+    uint256 playerBet;
+    uint public pot;
+    uint public ownerFee;
+    mapping(address => uint) public playerBalances;
     bool player1Won;
     bool player2Won;
     uint256 fee = 5; // 5%
 
     constructor() {
         owner = payable (msg.sender);
+        pot = 0;
+    }
+
+    //ownable function.  Change this to your wallet. 
+    function _canSetOwner() internal virtual view override returns (bool) {
+        return msg.sender == 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
     }
 
 
     function setPlayers(address payable _player1, address payable _player2) public {
-        require(msg.sender == owner, "Only the owner can set the players.");
+        //only 2 players at a time
         player1 = _player1;
         player2 = _player2;
     }
@@ -39,7 +52,7 @@ contract openBet {
     }
     
     Bet[] public bets;
-    uint public totalBetMoney = 0;
+    //uint public totalBetMoney += msg.value;
 
     mapping (address => uint) public numBetsAddress;
 
@@ -47,7 +60,7 @@ contract openBet {
         return bets[_winner].totalBetAmount;
         }
 
-    function createBet (address payable _player) external payable {       
+    function placeBet (address payable _player) external payable {       
         require (msg.sender != owner, "owner can't make a bet");
         require (numBetsAddress[msg.sender] == 0, "you have already placed a bet");
         require (msg.value > 0.01 ether, "bet more");
@@ -59,8 +72,9 @@ contract openBet {
         } else if (player2 == _player) {
             player2Bet += msg.value;
         } else {
-            custodialWallet.transfer(msg.value);
+            payableOwner.transfer(msg.value);
         }
+        pot += msg.value;
     }
 
     //function deposit(address payable _player) public payable {
@@ -76,22 +90,31 @@ contract openBet {
 
    
 
-    function setResult(bool _player1Won, bool _player2Won) public {
+    function setResult(bool _player1Won, bool _player2Won) payable public {
         require(msg.sender == owner, "Only the owner can set the result.");
         require(_player1Won != _player2Won, "Both players cannot win.");
         player1Won = _player1Won;
         player2Won = _player2Won;
 
-        if (player1Won) {
-            player1.transfer(player2Bet);
-            uint256 feeAmount = player2Bet * fee / 100;
-            custodialWallet.transfer(feeAmount);
-            payableOwner.transfer(player2Bet - feeAmount);
-        } else {
-            player2.transfer(player1Bet);
-            uint256 feeAmount = player1Bet * fee / 100;
-            custodialWallet.transfer(feeAmount);
-            payableOwner.transfer(player1Bet - feeAmount);
+        if (player1Won) 
+            ownerFee = pot / 20;
+            uint payout = pot - ownerFee;
+            player1.transfer(payout);
+            owner.transfer(ownerFee);
+           
+         if (player2Won) {
+            ownerFee = pot / 20;
+            payout = pot - ownerFee;
+            player2.transfer(payout);
+            owner.transfer(ownerFee);
         }
+    }
+
+    function playAgain() public {
+        //require(msg.sender == owner, "Only the owner can reset the contract.");
+        pot = 0;
+        ownerFee = 0;
+        player1Bet = 0;
+        player2Bet = 0;
     }
 }
