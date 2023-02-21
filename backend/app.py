2dc5f2d452bd22c75c952850e1558562e43c6136
CLOVETHEDESTROYER <90019717+CLOVETHEDESTROYER@users.noreply.github.com>
2023-02-20 23:08:46 -0700

    debugging localhost8000/mk  attribute error saying the images I have dont work properly, I will reinstall images

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,11 @@
 from flask import Flask, jsonify
 # https://flask-cors.readthedocs.io/en/latest/#using-json-with-cors
 from flask_cors import CORS, cross_origin
-#from mkTemplateMatch import Winner
+from mkTemplateMatch import Winner
+import cv2
+import numpy as np
+import os
+import json
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
@@ -13,7 +17,7 @@ CORS(app, support_credentials=True)
 # @cross_origin(supports_credentials=True)  #could use this for specific routes but dont need it if you use line 7 above.
 def winner():
     print('in this route')
-    data = {'Winner': ["Member1", "Member2", "Member3", "CacaBallZ", "nalga", "assman"]}
+    data = {'Winner': ["Member1", "Member2", "Member3", "CacaBallZ", "nalga", "assmane"]}
     return jsonify(data)
 
 
@@ -24,12 +28,12 @@ def testroute():
     data = {'Testing': ["testtest", "testtest2"]}
     return jsonify(data)
 
-#@app.route('/mk')
-#def mk():
-#    # You can make all of your routes here so they dont have to be in separate files. 
-#    print('inside open bet')
-#    data = {'Winner': Winner()}
-#    return jsonify(data)
+@app.route('/mk')
+def mk():
+    # You can make all of your routes here so they dont have to be in separate files. 
+    print('inside open bet')
+    result = Winner('left', 'right', 'notFound')
+    return result
 
 
 
--- backend/mkTemplate2.py
+++ backend/mkTemplate2.py
@@ -4,50 +4,34 @@ import os
 import json
 from flask import Flask
 
-
-
-
 # Load the image you want to detect
 template = cv2.imread('backend/assets/dots12.jpg', cv2.IMREAD_GRAYSCALE)
-w, h = template.shape[::1]
+#w, h = template.shape[::-1]
 #template = cv2.resize(img,(20, 50), fx=1, fy=1)
 
-def Winner(Left, Right, notFound): 
+def Winner():
     img = cv2.imread('backend/assets/dots12.jpg',0)
     template = cv2.imread('backend/assets/dot2.jpg',0)
-    #w, h = template.shape[::-1]
+    w, h = template.shape[::-1]
+    print (img)
 
-    #perform template matching
-    res= cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
+    # print template shape for debugging
+    print(f"template shape: {template.shape}")
 
-    # Get the maximum correlation coefficient and its position
+    # Perform template matching
+    res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
-    # Create a dictionary to store the winner information
-    winner = {}
+    # print max_val for debugging
+    print(f"max_val: {max_val}")
 
-    # If the image was detected in region 1, return "Left Side Wins"
-    if max_val1 > .2:
-        winner['winner'] = Left 
-        print("you Dad") 
-    # If the image was detected in region 2, return "Region 2"
-    elif max_val2 > .2:
-        winner['winner'] = Right
-        print("you Mom") 
-        
-    # If the image was not detected in either region, return "Not found"
-    else: 
-        winner['winner'] = notFound
-        print("aint shit")
+    # check if max_val is greater than 0.89
+    if max_val > 0.89:
+        return True
+    else:
+        return False
 
-    #add the maximum correlation coefficient to the dictionary
-    winner['correlation_coefficient'] = max_val1
 
-    # Return the winner dictionary
-    return winner
-        
-
-# Set up the video capture object
 cap = cv2.VideoCapture('backend/video/MK11video.mov', 0)
 width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
 height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
@@ -55,20 +39,18 @@ height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 # Print the size of the frame
 print(f'Frame size: {width} x {height}')
 
-while True:
+while (cap.isOpened()):
     # Capture frame-by-frame
-    ret, frame = cap.read()
-    #toggle the below code to test image on both sides of fram
-    frame = cv2.flip(frame, 1)
+     # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret:
+            assert not isinstance(frame,type(None)), 'frame not found'
 
     
 
-    # If the frame was not grabbed, break
-    if not ret:
-        break
 
     # Convert the frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 
     # Define the two detection regions 
@@ -80,39 +62,39 @@ while True:
     #right = gray[(275, 450), (2, 90)]
     #left = gray[25:0, 322:422]
     #right = gray[25:0, 480:580]
-    left = gray[0:20, 100:422]
-    right = gray[0:20, 480:580]
+left = gray[0:20, 100:422]
+right = gray[0:20, 480:580]
     
 
     # Detect the image in each region
-    result1 = cv2.matchTemplate(left, template, cv2.TM_CCORR_NORMED)
-    result2 = cv2.matchTemplate(right, template, cv2.TM_CCORR_NORMED)
+result1 = cv2.matchTemplate(left, template, cv2.TM_CCORR_NORMED)
+result2 = cv2.matchTemplate(right, template, cv2.TM_CCORR_NORMED)
     #result1 = cv2.matchTemplate(frame[25:322, 0:422], img, cv2.TM_CCORR_NORMED)
     #result2 = cv2.matchTemplate(frame[25:480, 0:580], img, cv2.TM_CCORR_NORMED)
 
 
     # Find the maximum value in each region
-    _, max_val1, _, _ = cv2.minMaxLoc(result1)
-    _, max_val2, _, _ = cv2.minMaxLoc(result2)
-    print(max_val1, max_val2)
+_, max_val1, _, _ = cv2.minMaxLoc(result1)
+_, max_val2, _, _ = cv2.minMaxLoc(result2)
+print(max_val1, max_val2)
 
 
     # If the image was detected in region 1, draw a rectangle around it
-    if max_val1 > .94068063:
+if max_val1 > .94068063:
         cv2.rectangle(frame, (322, 25), (422, 0), (0, 0, 255), 7)
     # If the image was detected in region 2, draw a rectangle around it
-    if max_val2 > .82:
+if max_val2 > .82:
         cv2.rectangle(frame, (480, 25), (580, 0), (255, 0, 0), 7)
 
 
     # If the image was detected in region 1, return "Left Side Wins"
-    if max_val1 > .89: 
+if max_val1 > .89: 
         print ("LEFT SIDE WINS")  
     # If the image was detected in region 2, return "Region 2"
-    elif max_val2 > .89: 
+elif max_val2 > .89: 
         print ("RIGHT SIDE WINS") 
     # If the image was not detected in either region, return "Not found"
-    else: 
+else: 
         print ("NOT FOUND")
         
 
@@ -130,8 +112,8 @@ while True:
     #imgT = cv2.rectangle(frame, (25, frame.shape[2]//4), (422, frame.shape[0]//4), (0, 255, 0), 5)
     #imgTR = cv2.rectangle(frame, (480, frame.shape[0]//4), (902,frame.shape[2]), (0, 0, 220), 5)
 
-    imgL = cv2.rectangle(frame, (322, 25), (422, 0), (255, 0, 0), 5)
-    imgR = cv2.rectangle(frame, (480, 25), (580, 0), (255, 255, 0), 5)
+imgL = cv2.rectangle(frame, (322, 25), (422, 0), (255, 0, 0), 5)
+imgR = cv2.rectangle(frame, (480, 25), (580, 0), (255, 255, 0), 5)
 
 
 
@@ -139,16 +121,16 @@ while True:
    
 
     # Show the frame
-    cv2.imshow('Frame', frame)
-    cv2.imshow('ROI', right)
+cv2.imshow('Frame', frame)
+cv2.imshow('ROI', right)
 
 
         
 
     # Check for user input
-    if cv2.waitKey(1) == ord('q'):
-        break
+if cv2.waitKey(1) == ord('q'):
+             
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
 
